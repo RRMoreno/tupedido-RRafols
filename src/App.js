949ab6436ae69a5b9a3e0560dcfbce6ec05b834e
Roxana M. Rafols Moreno <roxanarafols94@gmail.com>
2021-06-17 17:21:54 +0200

    agregado servidor para mockear api agregado axios para realizar llamadas api pedir los restaurantes a traves de una api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import {Restaurants} from "./api/restaurants";
+import React from "react";
+import axios from "axios";
+import Restaurant from "./models/restaurant";
 
 function App() {
-    const restaurants = Restaurants;
+    let [restaurants, setRestaurants] = React.useState([]);
+    React.useEffect(() => {
+        axios.get('/restaurant').then(response => {
+            let data = response.data.map(x => {
+                return new Restaurant(x);
+            });
+            setRestaurants(data);
+        });
+    }, []);
     return (
         <div className="App">
             <NavBar/>

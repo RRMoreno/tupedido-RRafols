090dafd4d1beeffd5538224c59440ed4bd82c1bd
Roxana M. Rafols Moreno <roxanarafols94@gmail.com>
2021-07-24 05:09:57 +0200

    Create Order

--- src/components/PaymentStep/PaymentStep.jsx
+++ src/components/PaymentStep/PaymentStep.jsx
@@ -1,14 +1,45 @@
-import React, {useContext} from 'react';
-import {Grid} from "@material-ui/core";
+import React, {useContext, useEffect, useState} from 'react';
+import {Grid, LinearProgress} from "@material-ui/core";
 import {CartContext} from "../../providers/CartProvider/CartProvider";
 import {CheckoutContext} from "../../providers/CheckoutProvider/CheckoutProvider";
+import firebase from "firebase";
+import {useAuth0} from "@auth0/auth0-react";
+import Typography from "@material-ui/core/Typography";
 
 function PaymentStep() {
-    const cartContext = useContext(CartContext);
+    const [loading, setLoading] = useState(true);
+    const [orderId, setOrderId] = useState(undefined);
+    const {cartItems} = useContext(CartContext);
     const checkoutContext = useContext(CheckoutContext);
+    const {user} = useAuth0();
+    const db = firebase.firestore();
+    useEffect(() => {
+        db.collection("orders").add({
+            items: cartItems,
+            email: user.email,
+            address: checkoutContext.address,
+            date: checkoutContext.date,
+            name: checkoutContext.name,
+            lastname: checkoutContext.lastname,
+            phone: checkoutContext.phone,
+        }).then(order => {
+            setTimeout(() => {
+                setLoading(false);
+                setOrderId(order.id);
+            }, 2000)
+        });
+    }, []);
     return (
         <Grid container direction={"column"}>
-
+            {loading && <div>
+                <Typography variant={"caption"}>Please wait while we create your order...</Typography>
+                <LinearProgress/>
+            </div>}
+            {
+                !loading && <div>
+                    <Typography color={"textSecondary"} variant={"caption"}>Your order: {orderId} was created successfully</Typography>
+                </div>
+            }
         </Grid>
     );
 }
